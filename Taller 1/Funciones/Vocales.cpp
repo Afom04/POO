79e649dc9263c3e117cbd4c3a527deec4f2e6c0c
Afom04 <115759944+Afom04@users.noreply.github.com>
2022-12-20 21:13:09 -0500

    Rename AndreyOrozcoVocales.cpp to Vocales.cpp

--- Taller 1/Funciones/Vocales.cpp
+++ Taller 1/Funciones/Vocales.cpp
@@ -6,16 +6,16 @@ using namespace std;
 void contar(char frase[50]);
 void posicion(int posa[50],int pose[50],int posi[50],int poso[50],int posu[50],int a,int e,int i,int o,int u);
 char llenar(char frase[50]);
-//Declaraci髇 globar de variables(arreglos) que se usaran
+//Declaraci贸n globar de variables(arreglos) que se usaran
 //char frase[50];
 //int posa[50],pose[50],posi[50],poso[50],posu[50];
 //Funcion principal
 int main() {
 	char frase[50];
-	llenar(frase);//Env韔 de el vector creado para llenarlo con espacios vac韔s ' ' para que no genere un valor o caracter que rompa el programa
+	llenar(frase);//Env铆o de el vector creado para llenarlo con espacios vac铆os ' ' para que no genere un valor o caracter que rompa el programa
 	cout<<"Por favor ingrese una frase"<<endl;
 	cin.getline(frase,50);//Se usa el "cin.getline" para que separe cada componente en un espacio del vector
-	contar(frase);//Env韔 del vector que contiene la frase a evaluar
+	contar(frase);//Env铆o del vector que contiene la frase a evaluar
 }
 char llenar(char frase[50]) {
 	for(int c=0; c<50; c++) {//Ciclo para llenar los espacios del vector para evitar errores
@@ -23,61 +23,61 @@ char llenar(char frase[50]) {
 	}
 	return frase[50];
 }
-void contar(char frase[50]) {//Funci髇 encargada de realizar el conteo de vocales y acomodar en los vectores las posiciones de las vocales, usando un acumulador
+void contar(char frase[50]) {//Funci贸n encargada de realizar el conteo de vocales y acomodar en los vectores las posiciones de las vocales, usando un acumulador
 	int contarv=0,a=0,e=0,i=0,o=0,u=0;//Definicion de variables que funcionan como contadores de cada vocal y de el total;
 	int posa[40],pose[40],posi[40],poso[40],posu[30];
 	for(int p=0; p<=40; p++) {//Ciclo for que recorre el vector que contiene la frase
-		if (frase[p]=='a'||frase[p]=='A') {//Condicional que se encarga de separar cada vocal, acumulandola y guardando su posici髇 en un vector ()
+		if (frase[p]=='a'||frase[p]=='A') {//Condicional que se encarga de separar cada vocal, acumulandola y guardando su posici贸n en un vector ()
 			contarv++;
 			posa[a]=p;
 			a++;
-		} else if (frase[p]=='e'||frase[p]=='E') {//Condicional que se encarga de separar cada vocal, acumulandola y guardando su posici髇 en un vector (E)
+		} else if (frase[p]=='e'||frase[p]=='E') {//Condicional que se encarga de separar cada vocal, acumulandola y guardando su posici贸n en un vector (E)
 			contarv++;
 			pose[e]=p;
 			e++;
-		} else if (frase[p]=='i'||frase[p]=='I') {//Condicional que se encarga de separar cada vocal, acumulandola y guardando su posici髇 en un vector (I)
+		} else if (frase[p]=='i'||frase[p]=='I') {//Condicional que se encarga de separar cada vocal, acumulandola y guardando su posici贸n en un vector (I)
 			contarv++;
 			posi[i]=p;
 			i++;
-		} else if (frase[p]=='o'||frase[p]=='O') {//Condicional que se encarga de separar cada vocal, acumulandola y guardando su posici髇 en un vector (O)
+		} else if (frase[p]=='o'||frase[p]=='O') {//Condicional que se encarga de separar cada vocal, acumulandola y guardando su posici贸n en un vector (O)
 			contarv++;
 			poso[o]=p;
 			o++;
-		} else if (frase[p]=='u'||frase[p]=='U') {//Condicional que se encarga de separar cada vocal, acumulandola y guardando su posici髇 en un vector (U)
+		} else if (frase[p]=='u'||frase[p]=='U') {//Condicional que se encarga de separar cada vocal, acumulandola y guardando su posici贸n en un vector (U)
 			contarv++;
 			posu[u]=p;
 			u++;
 		}
 	}
 	cout<<"La frase ingresada tiene un total de "<<contarv<<" vocales"<<endl;
-	posicion(posa,pose,posi,poso,posu,a,e,i,o,u);//Env韔 del acumulado de cada vocal
+	posicion(posa,pose,posi,poso,posu,a,e,i,o,u);//Env铆o del acumulado de cada vocal
 	cout<<"La frase que fue ingresada es : <<"<<frase<<">>";
 }
-void posicion(int posa[50],int pose[50],int posi[50],int poso[50],int posu[50],int a,int e,int i, int o, int u) {//Funci髇 encargada de mostrar la posici髇 de cada vocal y su acumulaci髇
+void posicion(int posa[50],int pose[50],int posi[50],int poso[50],int posu[50],int a,int e,int i, int o, int u) {//Funci贸n encargada de mostrar la posici贸n de cada vocal y su acumulaci贸n
 	if(a!=0) {//Condicional que evalua si hay una vocal especifica en la palabra por medio del acumulador
 		cout<<"La frase tiene "<<a<<" vocales A"<<endl<<"En las posiciones:";
-		for(int j=0; j<a; j++) {//Ciclo con el cual se recorre los vectores que guardan la posici髇 de cada vocal
+		for(int j=0; j<a; j++) {//Ciclo con el cual se recorre los vectores que guardan la posici贸n de cada vocal
 			cout<<"["<<posa[j]<<"] ";
 		}
 		cout<<endl;
 	}
 	if(e!=0) {//Condicional que evalua si hay una vocal especifica en la palabra por medio del acumulador
 		cout<<"La frase tiene "<<e<<" vocales E"<<endl<<"En las posiciones:";
-		for(int j=0; j<e; j++) {//Ciclo con el cual se recorre los vectores que guardan la posici髇 de cada vocal
+		for(int j=0; j<e; j++) {//Ciclo con el cual se recorre los vectores que guardan la posici贸n de cada vocal
 			cout<<"["<<pose[j]<<"] ";
 		}
 		cout<<endl;
 	}
 	if(i!=0) {//Condicional que evalua si hay una vocal especifica en la palabra por medio del acumulador
 		cout<<"La frase tiene "<<i<<" vocales I"<<endl<<"En las posiciones";
-		for(int j=0; j<i; j++) {//Ciclo con el cual se recorre los vectores que guardan la posici髇 de cada vocal
+		for(int j=0; j<i; j++) {//Ciclo con el cual se recorre los vectores que guardan la posici贸n de cada vocal
 			cout<<"["<<posi[j]<<"] ";
 		}
 		cout<<endl;
 	}
 	if(o!=0) {//Condicional que evalua si hay una vocal especifica en la palabra por medio del acumulador
 		cout<<"La frase tiene "<<o<<" vocales O"<<endl<<"En las posiciones";
-		for(int j=0; j<o; j++) {//Ciclo con el cual se recorre los vectores que guardan la posici髇 de cada vocal
+		for(int j=0; j<o; j++) {//Ciclo con el cual se recorre los vectores que guardan la posici贸n de cada vocal
 			cout<<"["<<poso[j]<<"] ";
 		}
 		cout<<endl;
@@ -85,7 +85,7 @@ void posicion(int posa[50],int pose[50],int posi[50],int poso[50],int posu[50],i
 
 	if(u!=0) {//Condicional que evalua si hay una vocal especifica en la palabra por medio del acumulador
 		cout<<"La frase tiene "<<u<<" vocales U"<<endl<<"En las posiciones";
-		for(int j=0; j<u; j++) {//Ciclo con el cual se recorre los vectores que guardan la posici髇 de cada vocal
+		for(int j=0; j<u; j++) {//Ciclo con el cual se recorre los vectores que guardan la posici贸n de cada vocal
 			cout<<"["<<posu[j]<<"] ";
 		}
 		cout<<endl;
